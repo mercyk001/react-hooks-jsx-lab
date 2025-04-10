1049166ed2b5567523c39b55c8c620d1f887fbb1
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-04-10 12:46:01 +0300

    react jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Passionate about creating web applications and learning new technologies.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
